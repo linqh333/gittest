1dae42a20dc26546f9715560a61f1fe1a2f40252
linqh <linqh@newlandcomputer.com>
2018-05-31 13:51:12 +0800

    添加gggggggggg

--- gittest/src/main/java/com/newland/bi/GitTest.java
+++ gittest/src/main/java/com/newland/bi/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("ddddddddd");
         System.out.println("fffffffffff");
         System.out.println("eeeeeee");
+        System.out.println("ggggggggggggg");
     }
 }

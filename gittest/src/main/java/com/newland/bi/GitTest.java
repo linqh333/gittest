9a119a43c509ac785a77c6c85aa11507cbd6cdee
linqh <linqh@newlandcomputer.com>
2018-05-31 08:52:50 +0800

    添加gittest模块

---
+++ gittest/src/main/java/com/newland/bi/GitTest.java
@@ -0,0 +1,11 @@
+package com.newland.bi;
+
+/**
+ * Created by linqh on 2018/5/31.
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello GIT!");
+    }
+}
